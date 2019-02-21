7d49df2c1f63fba2c647cac604659e1ed9978070
Attapong <attapong@attapongs-macbook-pro.local>
2019-02-21 22:00:53 +0700

    Initial Commit

---
+++ MapJSON/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapJSON
+//
+//  Created by Attapong on 21/2/2562 BE.
+//  Copyright © 2562 Attapong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
